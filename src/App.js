eaa968cb4721291f5f5b188f788a917d711b2e11
Brian Vaughn <bvaughn@fb.com>
2021-07-13 13:37:02 -0400

    Tweaked App file to add hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount(count - 1);
+  const increment = () => setCount(count + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={increment}>increment</button>
+      Count: {count}
+    </>
   );
 }
-
-export default App;
